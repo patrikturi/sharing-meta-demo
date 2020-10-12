2316d1156d892d1146cbe4591e6e509c35eb4082
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-10-12 22:04:13 +0200

    feat: add localized meta tags

--- mysite/sharing/views.py
+++ mysite/sharing/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
+SUPPORTED_LOCALES = ['en_us', 'hu_hu']
+
+
 def index(request):
-    return render(request, 'sharing/index.html', {})
+    current_locale = request.GET.get('fb_locale') or request.GET.get('locale', 'en_us')
+
+    return render(request, 'sharing/index.html', {
+        'current_locale': current_locale,
+        'supported_locales': SUPPORTED_LOCALES,
+    })
