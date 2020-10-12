7e8158b0dd55b309a3b9dd8cc2f56cf99eafd88a
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-10-12 23:08:04 +0200

    Change locale country to uppercase

--- mysite/sharing/views.py
+++ mysite/sharing/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 
-SUPPORTED_LOCALES = ['en_us', 'hu_hu']
+SUPPORTED_LOCALES = ['en_US', 'hu_HU']
 
 
 def index(request):
-    current_locale = request.GET.get('fb_locale') or request.GET.get('locale', 'en_us')
+    current_locale = request.GET.get('fb_locale') or request.GET.get('locale', 'en_US')
 
     return render(request, 'sharing/index.html', {
         'current_locale': current_locale,
